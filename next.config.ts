a3e315daa0094264bd638d2c9728a4deb5df7259
Sanket Mane <sanketpatil515151@gmail.com>
2026-02-01 03:46:23 +0530

    Fix: Add i.pravatar.cc to configured image hostnames

--- next.config.ts
+++ next.config.ts
@@ -65,6 +65,10 @@ const nextConfig: NextConfig = {
       {
           protocol: "https",
           hostname: "ui-avatars.com",
+      },
+      {
+          protocol: "https",
+          hostname: "i.pravatar.cc",
       }
     ],
   },
