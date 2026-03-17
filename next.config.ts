bf558a1f8cabf6d9f1e2f5d696aca0f6e269857e
Sanket Mane <sanketmane7170@gmail.com>
2026-03-17 10:00:14 +0530

    Increase body size limit to 50mb

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
 
   experimental: {
     serverActions: {
-      bodySizeLimit: '10mb', // Increase from default 1mb
+      bodySizeLimit: '50mb', // Increase from default 1mb
     },
   },
 
